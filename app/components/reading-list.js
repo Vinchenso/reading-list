6bdcbac1f99f28ce25ab88ce695083c6129be66b
Vinchenso <chensok@gmail.com>
2016-12-08 14:32:23 +0200

    data down, actions up componet that adds books

--- app/components/reading-list.js
+++ app/components/reading-list.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  value: '',
+
+  actions: {
+
+    onEnter(title){
+      if(title){
+        this.get('addBook')({ title });
+      }
+      this.set('value', '');
+    }
+
+  }
 });
